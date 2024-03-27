5d02c09b6eca861adf6fc4f86c2b6a96436e6d4a
Nikeeta26 <nikeetakaudare14@gmail.com>
2024-03-27 22:44:18 +0530

    Update Dom.js

---
+++ Dom.js
@@ -0,0 +1,97 @@
+
+/************getElementsByClassName************* */
+let s = document.getElementsByClassName("oldImg");
+for(let i=0;i<s.length;i++)
+{
+//console.dir(s[i]);
+    s[i].src="assets/spiderman_img.png" ;
+console.log(`value of image no. ${i} is change`);
+}
+
+/***************getElementsById*******************/
+let i = document.getElementById("mainImg");
+console.log(i);
+
+/****************getElementsByTagName*************** */
+let p = document.getElementsByTagName("p")[0].innerText="abc";
+console.log(p);
+
+/***************querySelector********************/
+console.dir(document.querySelector("h1"));//tagname
+
+console.log(document.querySelector("#mainImg"));
+
+console.dir(document.querySelector(".box a"));
+
+console.log(document.querySelectorAll('.images .oldImg'));
+/***********************innerText************ */
+let para = document.querySelector("#description");
+console.log(para.innerText="asdfghjklwertyuiopxcvbnm,sdfghjkertyui");
+
+
+/*******************innerHTML******************** */
+let par = document.querySelector("#description");
+console.log(para.innerHTML="asdfghjklwertyuiopxcvbnm,sdfghjkertyui");
+
+/***********************textConten*********************** */
+let pa = document.querySelector("#description");
+console.log(para.textContent="asdfghjklwertyuiopxcvbnm,sdfghjkertyui");
+
+/********************getElements and setElements************ */
+let g = document.querySelector(".images");
+console.log(g.getAttribute('class'));
+console.log(g.setAttribute('class','nikeeta'));
+console.log(g);
+
+/******************manipulating style**************** */
+let n=document.querySelector("h1");
+console.log(n.style.backgroundColor="pink");
+
+let links = document.querySelectorAll(".box a");
+for(let i of links)
+{
+    i.style.color="red";
+}
+// for(let i=0; i<links.length; i++)
+// {
+// links[i].style.color="pink";
+
+// }
+let m=document.querySelector('h1');
+console.log(m.classList.add("image"));//add
+
+console.log(m.classList.remove("image"));//remove
+
+console.log(m.classList.contains("image"));
+
+
+console.log(m.classList.toggle("image"));
+
+console.log(m.classList.toggle("new"));
+
+console.log(m.classList.toggle("new"));
+
+let b=document.querySelector(".box");
+console.log(b.classList.toggle("bgcolor"));
+
+
+/****************Navigation********** */
+let h4=document.querySelector("h4");
+console.log(h4.parentElement);
+
+let box = document.querySelector(".box");
+console.log(box.children);
+
+let ul=document.querySelector("ul");
+console.log(ul.children);
+console.log(ul.children[1].previousElementSibling);
+console.log(ul.children[1].nextElementSibling);
+
+let im=document.querySelector("img")
+im.previousElementSibling.style.color="yellow";
+
+let add=document.createElement('p');
+console.log(add.innerText="hi i am nikeeta kaudare...............");
+let boxx = document.querySelector(".box");
+boxx.appendChild(add);
+
